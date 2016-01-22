bb8aeda77d4244324f10d41a97e02b446d824e73
Administrator <Administrator>
2016-01-22 12:20:12 +0800

    词云批量生成

--- R/wordcloud/wordCloud.R
+++ R/wordcloud/wordCloud.R
@@ -9,12 +9,12 @@ for (x in file) {
   print(x)
   sheetNum <- sheetCount(x)
   for (i in 1:sheetNum){
-    path <- paste("./pngs/", x, ".png")
+    path <- paste("./pngs/", x, i,".png")
     png(file= path, bg="white",width = 480, height = 480)
     colors=brewer.pal(8,"Dark2")
     par(family='STXihei')
     
-    data <- read.xls(x, header = F, sheet = i, header = T)
+    data <- read.xls(x, sheet = i, header = T, encoding = "UTF-8")
     wordcloud(data[,1],data[,2],scale=c(5,0.3),min.freq=-Inf,max.words=60,colors=colors,random.order=F,random.color=F,ordered.colors=F)
     
     dev.off()
